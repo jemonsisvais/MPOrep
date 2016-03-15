f1f4bca631a5892831297b1cc0fc752ec2fecb7f
Jesus Molina <jemolinamonsivais@miners.utep.edu>
2016-03-15 14:20:36 -0600

    Create Administrator.java

---
+++ users/Administrator.java
@@ -0,0 +1,6 @@
+public class Administrator extends User{
+  //public void authenticate(String username, String passwd);
+  //public void createUserAccount();
+  //public void editUseraccount(User u);
+  //public void deleteUserAccount(User u);
+}
