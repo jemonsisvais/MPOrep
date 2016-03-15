de0cfb1a28ac8e66f8157b3f8a49d7c053400aa1
Jesus Molina <jemolinamonsivais@miners.utep.edu>
2016-03-15 14:11:12 -0600

    Create MPOStaff.java

---
+++ users/MPOStaff.java
@@ -0,0 +1,10 @@
+public class MPOStaff extends User{
+  //MPOStaff fields..
+  
+  //public void authenticate(String username, String passwd);
+  //public void approveSubmission(Project p);
+  //public void reviewSubmission(Project p);
+  //public void review model();
+  //public void Archive(Project p);
+  //public void manageProject(Project p);
+}
