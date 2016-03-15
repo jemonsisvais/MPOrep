6c0363cbcaeef1bab5efb5be5e80dd6db14036fd
Jesus Molina <jemolinamonsivais@miners.utep.edu>
2016-03-15 14:13:16 -0600

    Create ProjectLead.java

---
+++ users/ProjectLead.java
@@ -0,0 +1,6 @@
+public class ProjectLead extends User{
+  //public void authenticate(String username, String passwd);
+  //public void createSubmission();
+  //public void manageSubmission();
+  //public void manageProject();
+}
