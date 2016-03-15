d43e7d4c5c2d6948a94001f720105619374403d3
Jesus Molina <jemolinamonsivais@miners.utep.edu>
2016-03-15 14:17:50 -0600

    Create MPOLead.java

---
+++ users/MPOLead.java
@@ -0,0 +1,8 @@
+public class MPOLead extends User{
+  //public void authenticate(String username, String passwd);
+  //public void approveSubmission(Project p);
+  //public void manageUserAccount(User u);
+  //public void reviewSubmission(Project p);
+  //public void reviewModel();
+  //public void archiveProject(Project p);
+}
