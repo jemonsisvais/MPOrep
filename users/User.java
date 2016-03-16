f83f972f5b8193745eb8a85a1c270276f0f7032b
Jesus Molina <jemolinamonsivais@miners.utep.edu>
2016-03-16 11:18:15 -0600

    Update User.java

--- users/User.java
+++ users/User.java
@@ -1,5 +1,10 @@
-public class User{
-  //public Report generateReport();
-  //public void viewProject();
-  //public void viewReport();
+public class User
+{
+  /* Description: A superclass that manages the information and permissions of a user
+   *
+   * Common responsibilities in subclasses:
+   * Generate report  Collaborates with Report, ReportDatabase
+   * View project Collaborates with ProjectDatabase, GUI
+   * View report Collaborates with Gui, ReportDatabase
+   */
 }
