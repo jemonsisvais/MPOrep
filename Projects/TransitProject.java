4840bd386296a5fb133b2d84d668572cfa2bf566
calvoluis <lacalvo@miners.utep.edu>
2016-03-16 11:23:08 -0600

    Update TransitProject.java

--- Projects/TransitProject.java
+++ Projects/TransitProject.java
@@ -1,3 +1,13 @@
-public class TransitProject extends Project {
-  //transit project fields..
+public class TransitProject extends Project
+{
+  
+  /* Description: Manages the information of a funded project
+   * 
+   * Unique responsibilities:
+   * Knows Apportionment year
+   * Knows Section 5309ID
+   * Knows TCD Amount requested
+   * Knows Transit project type
+   */
+  
 }
