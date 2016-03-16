7278c3c21e2a12f4383125409b4e13fe003fc9b9
calvoluis <lacalvo@miners.utep.edu>
2016-03-16 11:25:32 -0600

    Update TexasTransitCategory.java

--- fundingcategories/TexasTransitCategory.java
+++ fundingcategories/TexasTransitCategory.java
@@ -1,4 +1,19 @@
-public class TexasTransitCategory extends FundingCategory{
-  //Texas Transit relevant information..
-  
+public class TexTransit extends FundingCategory
+{
+  /*Description: Manages Texas transit funding category information
+   * 
+   * Unique responsibilities:
+   * Knows the following fields: 
+   *  The subtotal for the seven categories in the large urban cities (within the concept of 5307).
+   *  The subtotal of transportation for Elderly Persons, Persons Disabilities Program, New Freedom Program (within the concept of 5310),
+   *    and theRural Transit Assistance Program (within the concept of 5311(b)(3)).
+   *  Subtotal of the Estimated Local Funding (SM) including sales tax, parking garages, fares, and alternative fuel credit.
+   *  Subtotal for Facilities and Equipment Funds.
+   *  Total of Transit Revenues
+   *  Construction Projects Cost includes PE and ROW costs 2013-2016.
+   *  Construction Operations Cost.
+   *  RE Cost.i.ROW Cost. 
+   *  Total Projects Costs.
+   *  Total Transit Balance with carry over.
+  */
 }
