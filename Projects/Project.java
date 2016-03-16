1d0b315559b500f18f10f0dd07d4c279533dfede
calvoluis <lacalvo@miners.utep.edu>
2016-03-16 11:21:22 -0600

    Update Project.java

--- Projects/Project.java
+++ Projects/Project.java
@@ -1,7 +1,24 @@
-public class Project{
-  //Organization organizationType
-  //common elements
-  //FundingCategory fundingCategory
-  //costs fields
-  //metadata fields
+public class Project
+{
+ /* Description: Manages the information for a single project object
+   * Responsibilities:
+   * Knows organization type Collaborates with Organization class
+   * Knows common elements of projects
+   * Knows funding category Collaborates with FundingCategory class
+   * Knows cost fields: 
+   *     Construction cost
+   *     Construction engineering cost
+   *     Contingencies cost
+   *     FTA transfer cost
+   *     Non Construction cost
+   *     Potential change cost
+   *     Preliminary engineering cost
+   *     Right of way cost
+   * Knows metadata fields: 
+   *     change date
+   *     comments
+   *     identifier 
+   *     UserID
+   *     Workflow status.
+   */ 
 }
