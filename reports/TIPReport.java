6fdbac9583e5a76bd951b29ed36806ff3917a2fe
malanda <malanda@miners.utep.edu>
2016-03-16 11:22:06 -0600

    Update TIPReport.java

--- reports/TIPReport.java
+++ reports/TIPReport.java
@@ -1,3 +1,31 @@
 public class TIPReport extends Report{
-  //TIP report unique fields..
+  /*Description: Manages TIP report information, subclass of Report
+   * 
+   * Unique responsibilities:
+   * Know TIP fields: 
+   *   Capacity project
+   *   City
+   *   County
+   *   DOT district
+   *   Federal fiscal year
+   *   Federal functional classification
+   *   Fund project type
+   *   Highway/roadway name
+   *   Limit from 
+   *   Limit to
+   *   Network year
+   *   Number of existing lanes
+   *   Number of miles
+   *   Number of projected lanes
+   *   Project description
+   *   Project ID
+   *   TIP project name
+   *   Project phases
+   *   Project type
+   *   Remarks
+   *   Sponsor agency
+   *   Transit information
+   *   Project cost
+   *   Project funding
+   */
 }
