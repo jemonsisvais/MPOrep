78e6a45d3d0b43e605513042a537edbe3881090b
Jesus Molina <jemolinamonsivais@miners.utep.edu>
2016-03-16 11:19:22 -0600

    Update Administrator.java

--- users/Administrator.java
+++ users/Administrator.java
@@ -1,6 +1,11 @@
-public class Administrator extends User{
-  //public void authenticate(String username, String passwd);
-  //public void createUserAccount();
-  //public void editUseraccount(User u);
-  //public void deleteUserAccount(User u);
+public class Administrator extends User
+{
+  /* Description: A subclass of user that manages the information of an administrator
+   * 
+   * Unique responsibilities:
+   * Authenticate Collaborates with UserDatabase
+   * Create user accouns Collaborates with UserDatabase
+   * Edit user account Collaborates with UserDatabase
+   * Delete user account Collaborates with UserDatabase
+   */
 }
