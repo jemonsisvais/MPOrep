d2b25501270345856ad3521d0fedd852627bb6ac
Jesus Molina <jemolinamonsivais@miners.utep.edu>
2016-03-16 11:20:07 -0600

    Update MPOLead.java

--- users/MPOLead.java
+++ users/MPOLead.java
@@ -1,8 +1,15 @@
-public class MPOLead extends User{
-  //public void authenticate(String username, String passwd);
-  //public void approveSubmission(Project p);
-  //public void manageUserAccount(User u);
-  //public void reviewSubmission(Project p);
-  //public void reviewModel();
-  //public void archiveProject(Project p);
+public class MPOLead extends User
+{
+  /* Description: A subclass of user that manages the information of the MPO lead
+   * 
+   * Unique responsibilities:
+   * Authenticate  Collaborates with UserDatabase
+   * Create submission Collaborates with Project, ProjectDatabase
+   * Manage submission Collaborates with ProjectDatabase
+   * Approve submission Collaborates with ProjectDatabase
+   * Manage user accounts Collaborates with UserDatabase
+   * Review submission Collaborates with ProjectDatabase, GUI
+   * Review model Collaborates with GUI
+   * Archive, Collaborates with Database
+   */
 }
