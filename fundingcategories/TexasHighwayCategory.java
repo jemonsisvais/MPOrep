1ccf736dd13384c1a50f406da9d0377a6f146155
calvoluis <lacalvo@miners.utep.edu>
2016-03-16 11:25:10 -0600

    Update TexasHighwayCategory.java

--- fundingcategories/TexasHighwayCategory.java
+++ fundingcategories/TexasHighwayCategory.java
@@ -1,4 +1,18 @@
-public class TexasHighwayCategory extends FundingCategory{
-  //Texas Highway relevant information..
+public class TexHighway extends FundingCategory
+{
+  /*Description: Manages Texas Highway funding category information
+   * 
+   * Unique responsibilities:
+   * Knows the following fields: 
+   *  Total TX Highway Revenues for each year 
+   *  Construction Cost for each year. 
+   *  The construction cost includes FHWA to FTA transfers, local and state PE for 2013 to 2016, and local and state ROW for 2013 to 2016.
+   *  Total 2013-2018 non-carry over.
+   *  In the MTP the non-carry over categories are defined in the Hwy TX Financials, Transit Financials, and NM Financials.
+   *  FHWA to FTATransfers
+   *  State and Local PE Cost
+   *  State and Local ROW Cost
+   *  Total Project Cost is the total amount added from construction cost, PE cost, and ROW cost.
+   *  Total Balance includes the carry over funding categories.
   
 }
