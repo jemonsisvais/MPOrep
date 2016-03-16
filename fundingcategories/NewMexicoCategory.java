453c0e1787ec190d0bbec61703b81206fb961dd1
calvoluis <lacalvo@miners.utep.edu>
2016-03-16 11:24:49 -0600

    Update NewMexicoCategory.java

--- fundingcategories/NewMexicoCategory.java
+++ fundingcategories/NewMexicoCategory.java
@@ -1,4 +1,13 @@
-public class NewMexicoCategory extends FundingCategory{
-  //New Mexico information..
-  
+public class NewMexico extends FundingCategory
+{
+  /*Description: Manages New Mexico funding category information
+   * 
+   * Unique responsibilities:
+   * Knows the following fields: 
+   *  Total NM Roadway Revenues.
+   *  Total Construction Cost includes PE and ROW costs for 2013-2015.
+   *  PE Cost.d.ROW Cost.
+   *  Total Projects Costs
+   *  Total NM Balance with carry over.
+  */
 }
