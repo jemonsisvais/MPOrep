4c0bcd9e60e5b313bcf0024a2ca8a2f81260a2c4
calvoluis <lacalvo@miners.utep.edu>
2016-03-16 11:20:22 -0600

    Update CapacityProject.java

--- Projects/CapacityProject.java
+++ Projects/CapacityProject.java
@@ -1,6 +1,13 @@
-public class CapacityProject extends Project{
-  //int existingLanes;
-  //int proposedLanes;
-  //int miles;
-  //String[] highwayRoadwayNames;
+public class CapacityProject extends Project
+{
+  
+  /* Description: Manages the information of a capacity project
+   * 
+   * Unique responsibilities:
+   * Know number of existing lanes.
+   * Know number of projected lanes.
+   * Know number of miles.
+   * Know highway roadway names. 
+   */
+  
 }
