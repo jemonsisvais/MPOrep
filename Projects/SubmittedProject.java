0824adf878bad985337e47d0eba3e73cb07a20df
calvoluis <lacalvo@miners.utep.edu>
2016-03-16 11:22:33 -0600

    Update SubmittedProject.java

--- Projects/SubmittedProject.java
+++ Projects/SubmittedProject.java
@@ -1,3 +1,13 @@
-public class SubmittedProject extends Project{
-  //submited project fields..
+public class SubmittedProject extends Project
+{
+  
+  /* Description: Manages the information of a submitted project
+   * 
+   * Unique responsibilities:
+   * Knows submition status
+   * Knows State approval state
+   * Knows Federal approval state
+   * Knows TBP approval state
+   */
+  
 }
