b93d16635d6ef5e54452dbf4552f8e4449fe3bc2
Jesus Molina <jemolinamonsivais@miners.utep.edu>
2016-03-16 11:24:25 -0600

    Update ReportDatabase.java

--- databases/ReportDatabase.java
+++ databases/ReportDatabase.java
@@ -1,4 +1,8 @@
 public class ReportDatabase extends Database{
+  /* Description: Centralized storage for all reports
+   * Responsibilities are covered in Database superclass where entries are reports
+   */
+   
   //addEntry(Report r);
   //removeEntry(Report r);
   //editEntry(Report r);
