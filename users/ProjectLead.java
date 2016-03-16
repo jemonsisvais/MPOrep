3172a7f90ee5939b5de5381adae62e2b032cbacb
Jesus Molina <jemolinamonsivais@miners.utep.edu>
2016-03-16 11:21:46 -0600

    Update ProjectLead.java

--- users/ProjectLead.java
+++ users/ProjectLead.java
@@ -1,6 +1,11 @@
-public class ProjectLead extends User{
-  //public void authenticate(String username, String passwd);
-  //public void createSubmission();
-  //public void manageSubmission();
-  //public void manageProject();
+public class ProjectLead extends User
+{
+  /* Description: A subclass of user that manages the information of a project lead
+   * 
+   * Unique responsibilities:
+   * Authenticate Collaborates with UserDatabase
+   * Create submission Collaborates with Project, ProjectDatabase
+   * Manage Submission  Collaborates with ProjectDatabase
+   * Manage projects  Collaborates with ProjectDatabase
+   */
 }
