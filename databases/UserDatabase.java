c506f5ba8b1980e2784aed021cc45dbc13df616e
Jesus Molina <jemolinamonsivais@miners.utep.edu>
2016-03-16 11:24:50 -0600

    Update UserDatabase.java

--- databases/UserDatabase.java
+++ databases/UserDatabase.java
@@ -1,4 +1,8 @@
 public class UserDatabase extends Database{
+  /* Description: Centralized storage for all user information
+   * Responsibilities are covered in Database superclass where entries are users
+   */
+   
   //addEntry(User u);
   //removeEntry(User u);
   //editEntry(User u);
